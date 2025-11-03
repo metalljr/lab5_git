a46c94087ea2715ba5a2c024b58bb01c4b0f2ff8
Кулешов Кирилл <youexample.com>
2025-11-03 18:54:20 +0300

    Add array and matrix headers (v18)

---
+++ array_utils.h
@@ -0,0 +1,4 @@
+#ifndef ARRAY_UTILS_H
+#define ARRAY_UTILS_H
+int flip_max_abs_array(int *a, int n); // меняет знак элементов с макс. модулем (В18)
+#endif
---
+++ matrix_utils.h
@@ -0,0 +1,4 @@
+#ifndef MATRIX_UTILS_H
+#define MATRIX_UTILS_H
+int flip_max_abs_in_each_row(int **m, int r, int c); // по строкам (В18)
+#endif
